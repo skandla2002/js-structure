bdc602fbd0d9705f366507d31009606c453ecdd3
skandla2002 <skandla2002@gmail.com>
2021-01-11 01:02:50 +0900

    LinkedList 및 Tree 작성 준비

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import List from "./structures/List";
 import Stack from "./structures/Stack";
 import Queue from "./structures/Queue";
+import LList from "./structures/LinkedList";
+import Tree from "./structures/가";
 
 class struct {
   constructor() {
     this.List = List;
     this.Stack = Stack;
     this.Queue = Queue;
+    this.LList = LList;
+    this.Tree = Tree;
   }
 }
 module.exports = struct;
